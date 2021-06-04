36e5d350be50191e6c3834698d4eb39f39994966
RuanSalles <contatoruansales@gmail.com>
2021-06-04 18:39:00 -0300

    Deploy

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css'
+import Relogio from './componentes/Relogio'
+export default function App() {
+  return (
+    
+    <div className="relogio">
+      <Relogio/>
+    </div>
+  );
+}
